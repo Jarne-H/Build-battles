7fe1461fcbba355f2bcfe9cddfed8f2bb1075192
Jarne_H <47690466+jarnejens@users.noreply.github.com>
2023-07-01 11:04:40 +0200

    fix: go from config.json to config.php

---
+++ src/hidden/config/config.php
@@ -0,0 +1,6 @@
+<?php
+    $DB_SERVER = "ID387393_builders.db.webhosting.be";
+    $DB_USERNAME = "ID387393_builders";
+    $DB_PASSWORD = "48zjH4elmQ5z20L96I17";
+    $DB_NAME = "ID387393_builders";
+?>
